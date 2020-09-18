94fb7889f8bb666a79ac9d2066badb90368afd08
sandy ielegems <sandy.ielegems@outlook.com>
2020-09-18 09:10:52 +0200

    github pages

--- includes/footer.php
+++ includes/footer.php
@@ -52,9 +52,9 @@
 
                 <p>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    <!-- Copyright &copy;<script>
+                    Copyright &copy;<script>
                         document.write(new Date().getFullYear());
-                    </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> -->
+                    </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
